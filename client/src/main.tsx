31d8098926579a9f089c1b8dd496b2112b25b62d
shinking02 <k.shin0102@gmail.com>
2023-07-15 15:35:59 +0900

    react追加

---
+++ client/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const app = document.getElementById("app");
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>Hello World</h1>
+    </React.StrictMode>,
+    app
+)
