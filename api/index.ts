c5bbba25373d668468bd0486b4e3d9844b48af2b
shinking02 <k.shin0102@gmail.com>
2024-04-04 22:38:27 +0900

    Add express API endpoint for retrieving post by slug

---
+++ api/index.ts
@@ -0,0 +1,17 @@
+import express, { Router } from "express";
+
+const app = express();
+const router = Router();
+
+router.get("/posts/:slug", (req, res) => {
+    res.json({
+        post: {
+            title: "Test Post",
+            slug: req.params["slug"],
+        },
+    });
+});
+
+app.use("/api/express", router);
+
+export default app;
