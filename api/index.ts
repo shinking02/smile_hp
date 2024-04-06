ef4866e5ee4789b8b9c4d3dca53810e8fc64e899
shinking02 <k.shin0102@gmail.com>
2024-04-06 20:10:58 +0900

    Add function to display directory contents on API root

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,26 @@
+import fs from "fs";
+
 import express, { Router } from "express";
 
 const app = express();
 const router = Router();
 
+function displayDirectoryContents(path: string) {
+    fs.readdir(path, (err, files) => {
+        if (err) {
+            console.error("Error reading directory:", err);
+            return;
+        }
+
+        console.log("Contents of directory:", path);
+        files.forEach((file) => {
+            console.log(file);
+        });
+    });
+}
+
 router.get("/", (_, res) => {
+    displayDirectoryContents(process.cwd());
     res.send("Express on Vercel");
 });
 
