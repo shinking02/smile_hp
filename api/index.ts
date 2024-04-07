99138f5f07aa3eaba007a2dec746babe48c4b668
shinking02 <k.shin0102@gmail.com>
2024-04-07 21:58:30 +0900

    Update comments and include files in vercel.json

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
 import express, { Router } from "express";
 
-import { handleBlogs } from "./handler/_blogs";
+// import { handleBlogs } from "./handler/_blogs";
 
 const app = express();
 const router = Router();
 
-router.get("/blogs", handleBlogs);
+// router.get("/blogs", handleBlogs);
 
 app.use("/api", router);
 
