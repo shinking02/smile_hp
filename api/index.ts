e667d3dfd266ad4e918f6d067982980370965507
shinking02 <k.shin0102@gmail.com>
2024-04-07 22:11:51 +0900

    Remove unused fs and path imports and listDirectoryContents function

--- api/index.ts
+++ api/index.ts
@@ -6,29 +6,6 @@ const app = express();
 const router = Router();
 
 router.get("/blogs", handleBlogs);
-import * as fs from "fs";
-import * as path from "path";
-
-function listDirectoryContents(directoryPath: string) {
-    const contents = fs.readdirSync(directoryPath);
-
-    contents.forEach((content) => {
-        const contentPath = path.join(directoryPath, content);
-        const stats = fs.statSync(contentPath);
-
-        if (stats.isFile()) {
-            console.log(contentPath); // ファイルの場合はconsole.log
-        } else if (stats.isDirectory()) {
-            console.log(contentPath + " (directory)"); // ディレクトリの場合はconsole.logし、再帰的に処理
-            listDirectoryContents(contentPath);
-        }
-    });
-}
-
-router.use("/", () => {
-    console.log(process.cwd());
-    listDirectoryContents("/var/task/blogs");
-});
 
 app.use("/api", router);
 
