28113c12c8cb4c3b28919e78bac576af93593c62
shinking02 <k.shin0102@gmail.com>
2024-04-07 16:57:19 +0900

    test render

--- src/server/main.ts
+++ src/server/main.ts
@@ -7,6 +7,6 @@ app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+ViteExpress.listen(app, 3001, () =>
+  console.log("Server is listening on port 3000...")
 );
