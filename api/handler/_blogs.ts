c8ac76339c7c8e29cc711aea024668e9a61a6a89
shinking02 <k.shin0102@gmail.com>
2024-04-10 20:27:50 +0900

    Add thumbnailPath to Blog interface and handle its retrieval

--- api/handler/_blogs.ts
+++ api/handler/_blogs.ts
@@ -11,15 +11,41 @@ interface Blog {
     title: string;
     date: string; //YYYY-MM-DD
     markdown: string;
+    thumbnailPath: string;
 }
 
 export function handleBlogs(req: express.Request, res: express.Response) {
-    // const [size, page, topRequest] = [req.query.size, req.query.page, req.query.topRequest];
+    const PAGE_SIZE = 12;
+    const [page, topRequest] = [req.query.page, req.query.topRequest];
+    const pageNumber = parseInt(page as string) || 0;
+    const startIndex = (() => {
+        if (topRequest === "yes") {
+            return 0;
+        }
+        return pageNumber * PAGE_SIZE;
+    })();
+    const endIndex = topRequest === "yes" ? 3 : startIndex + PAGE_SIZE;
     const blogDirectories = fs.readdirSync(`${process.cwd()}/blogs`);
-    console.log(blogDirectories);
+    const hasNext = endIndex < blogDirectories.length;
+    const targetDirectories = blogDirectories
+        .map((dir) => `${process.cwd()}/blogs/${dir}`)
+        .slice(startIndex, endIndex);
+
+    const blogs: Blog[] = targetDirectories.map((dir) => {
+        const markdown = fs.readFileSync(`${dir}/blog.md`, "utf-8");
+        const meta = fs.readFileSync(`${dir}/meta.json`, "utf-8");
+        const { title, date, thumbnailPath } = JSON.parse(meta);
+        return {
+            title,
+            date,
+            markdown,
+            thumbnailPath,
+        };
+    });
+
     const response: BlogsResponse = {
-        blogs: [],
-        hasNext: false,
+        blogs,
+        hasNext,
     };
     res.send(response);
 }
