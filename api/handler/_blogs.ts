0b3d4625367beba63fd0aad73e903fe61cc69845
shinking02 <k.shin0102@gmail.com>
2024-04-10 15:32:47 +0900

    Add new BlogsResponse and Blog interfaces. Initialize response in handleBlogs

--- api/handler/_blogs.ts
+++ api/handler/_blogs.ts
@@ -1,5 +1,20 @@
 import express from "express";
 
+interface BlogsResponse {
+    blogs: Blog[];
+    hasNext: boolean;
+}
+
+interface Blog {
+    title: string;
+    date: string; //YYYY-MM-DD
+    markdown: string;
+}
+
 export function handleBlogs(req: express.Request, res: express.Response) {
-    res.send("Hello, blogs!");
+    const response: BlogsResponse = {
+        blogs: [],
+        hasNext: false,
+    };
+    res.send(response);
 }
